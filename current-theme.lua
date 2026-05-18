f5f0acd0aaf8b6ca5b0c40e51d23b5b17c114a6f
neros29 <sorenmile29@gmail.com>
2026-05-18 16:51:55 -0600

    chore(.gitignore): remove temperary current-theme file from git

--- current-theme.lua
+++ current-theme.lua
@@ -1,3 +1,3 @@
 
-hl.config({ general = { col = { active_border = "#adc6ff", inactive_border = "#3f4759" } } })
+hl.config({ general = { col = { active_border = "#b2c5ff", inactive_border = "#404659" } } })
         
