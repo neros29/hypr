bdbbfb1ccb6f944486bc4ebf82d938c5fa157c5e
neros29 <sorenmile29@gmail.com>
2026-05-18 16:52:44 -0600

    fix(moniters): fix the scaling issue's caused by 1.33 scale by changing to the slighty smaller 1.2 scale

--- modules/moniters.lua
+++ modules/moniters.lua
@@ -1,5 +1,5 @@
 -- env = GDK_SCALE,1
 
-hl.monitor({ output = "eDP-2", mode = "1920x1080@144", position = "auto", scale = 1.33 })
+hl.monitor({ output = "eDP-2", mode = "1920x1080@144", position = "auto", scale = 1.2 })
 hl.monitor({ output = "DP-1", mode = "1920x1080", position = "auto", scale = 1 })
 hl.monitor({ output = "HDMI-A-1", mode = "preferred", position = "auto-right", scale = 1 })
